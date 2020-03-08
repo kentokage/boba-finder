251fe9b3e9a8402f069d388ea8e18b44198d9273
Kenny To <kenny.to7@wipro.com>
2020-03-08 12:25:27 -0700

    Updates

--- precache-manifest.c51869af7b1bb000f7db18011aa2c19d.js
+++ precache-manifest.c51869af7b1bb000f7db18011aa2c19d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c06cea2ee9d23e8b23471176bc669867",
+    "revision": "00379005b8b625f0b5c87f538181cafb",
     "url": "/boba-finder/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/boba-finder/static/css/2.563af9af.chunk.css"
   },
   {
-    "revision": "ce71d8a884bea63cd6d8",
+    "revision": "996095a1c159369a7094",
     "url": "/boba-finder/static/css/main.fc90685c.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/boba-finder/static/js/2.a4995d09.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ce71d8a884bea63cd6d8",
-    "url": "/boba-finder/static/js/main.7aeb01a9.chunk.js"
+    "revision": "996095a1c159369a7094",
+    "url": "/boba-finder/static/js/main.928f240c.chunk.js"
   },
   {
     "revision": "d7fe1651d9b8dcff09cc",
