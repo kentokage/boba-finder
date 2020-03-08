2f9bb0f2e86d6562f78181084d6d3e2ffa5db324
Kenny To <kenny.to7@wipro.com>
2020-03-08 12:06:59 -0700

    Updates

---
+++ precache-manifest.bab8f8f9f7d26cdb4cfb407d1ee1f386.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06cea2ee9d23e8b23471176bc669867",
+    "url": "/boba-finder/index.html"
+  },
+  {
+    "revision": "6919282f0d3a15c1f093",
+    "url": "/boba-finder/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "ce71d8a884bea63cd6d8",
+    "url": "/boba-finder/static/css/main.fc90685c.chunk.css"
+  },
+  {
+    "revision": "6919282f0d3a15c1f093",
+    "url": "/boba-finder/static/js/2.a4995d09.chunk.js"
+  },
+  {
+    "revision": "8e6fc7135ff95b6eefd686ac4899f67a",
+    "url": "/boba-finder/static/js/2.a4995d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce71d8a884bea63cd6d8",
+    "url": "/boba-finder/static/js/main.7aeb01a9.chunk.js"
+  },
+  {
+    "revision": "d7fe1651d9b8dcff09cc",
+    "url": "/boba-finder/static/js/runtime-main.18eed34d.js"
+  }
+]);
